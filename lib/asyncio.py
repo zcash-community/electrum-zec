e1164e56588a2f1d3874cd12a702a751e51080ce
Janus <ysangkok@gmail.com>
2017-11-30 11:30:38 +0100

    asyncio: get transactions from history

--- lib/asyncio.py
+++ lib/asyncio.py
@@ -9,22 +9,33 @@ async def read_reply(reader):
     while True:
       obj += await reader.read(1)
       try:
-        return json.loads(obj.decode("ascii"))
+        return json.loads(obj.decode("ascii"))["result"]
       except ValueError:
         continue
 
 def rpc_request(method, *params):
     return json.dumps({"jsonrpc": "2.0", "id": 1, "params": params, "method": method}).encode("ascii") + b"\n"
 
+def get_request_response_fun(reader, writer):
+    async def send_request_get_response(method, *params):
+        writer.write(rpc_request(method, *params))
+        await writer.drain()
+        return await read_reply(reader)
+    return send_request_get_response
+
 async def slow_operation(future):
     reader, writer = await asyncio.open_connection("148.251.87.112", 51002)
+    req = get_request_response_fun(reader, writer)
+
     hash = bitcoin.address_to_scripthash("mttiY9WTf17eNgdwJ8zJPg3ETd4wcUY7o4")
-    writer.write(rpc_request("server.version", "1.1", "1.1"))
-    await writer.drain()
-    print(await read_reply(reader))
-    writer.write(rpc_request("blockchain.scripthash.get_history", hash))
-    await writer.drain()
-    print(await read_reply(reader))
+    print(await req("server.version", "1.1", "1.1"))
+    hash_height_pairs = await req("blockchain.scripthash.get_history", hash)
+    print(hash_height_pairs)
+    for dict in hash_height_pairs:
+        # also available: dict["height"]
+        print(dict["tx_hash"])
+        print(await req("blockchain.transaction.get", dict["tx_hash"]))
+    writer.close()
     future.set_result('Future is done!')
 
 def asyncio_test(thiswallet):
